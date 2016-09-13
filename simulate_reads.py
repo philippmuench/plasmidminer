b4753db92c97c487a6c657dde65249da1661c00e
philippmuench <philipp.munech@helmholtz-hzi.de>
2016-09-13 14:28:12 +0200

    exporting multiple fasta files

--- simulate_reads.py
+++ simulate_reads.py
@@ -23,12 +23,13 @@ def chunks(l, n):
 	for i in xrange(0, len(l), n):
 		yield l[i:i+n]
 
-def split(length, path):
+def split(length, path, export):
+	print("create chunks")
 	for filename in glob.iglob(path):
-		print(filename)
 		(prefix, sep, suffix) = filename.rpartition('.')
 		new_filename = prefix + '.frag.fasta'	
 		if __name__ == '__main__':
+			# extract chunks
 			handle = open(filename, 'r')
 			records = list(SeqIO.parse(handle, "fasta"))
 			record = records[0]
@@ -36,15 +37,19 @@ def split(length, path):
 			for (pos, chunk) in enumerate(chunks(str(record.seq), splitlength)):
 	    			records.append(SeqRecord(Seq(chunk, record.seq.alphabet), id=str(pos), name=record.name, description=record.description))    	
 	 		SeqIO.write(records, open(new_filename, 'w'), "fasta")
+	 		# create multiple sequence fasta file
+	print("exporting " + export)
+    	with open(export, 'w') as outfile:
+        	for fname in glob.iglob(path):
+        		with open(fname) as infile:
+        			for line in infile:
+        				outfile.write(line)
 
 pla_path = 'pla/*.fasta'
+pla_path_export = "plasmid_200.fasta"
 chr_path = 'chr/*.fasta'
+chr_path_export = "chromosomes_200.fasta"
 
 splitlength = 200 # set length of outpuf fragments
-split(splitlength, pla_path)
-split(splitlength, chr_path)
-
-#print("merge files")
-#os.system('cat pla/*.frag.fasta > plasmids_200.fasta')
-#os.system('cat chr/*.frag.fasta > chromosomes_200.fasta')
-
+split(splitlength, pla_path, pla_path_export)
+split(splitlength, chr_path, chr_path_export)
