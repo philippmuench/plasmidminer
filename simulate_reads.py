3fbffe73ba2be136ea5ff844e0f83ced54b0123d
philippmuench <philipp.munech@helmholtz-hzi.de>
2016-09-13 14:21:41 +0200

    bugfix

--- simulate_reads.py
+++ simulate_reads.py
@@ -24,20 +24,18 @@ def chunks(l, n):
 		yield l[i:i+n]
 
 def split(length, path):
-	files =  glob.iglob(path)
-	#bar = Bar('split plasmid sequences', max=len(files))
-	for filename in files:
+	for filename in glob.iglob(path):
 		print(filename)
 		(prefix, sep, suffix) = filename.rpartition('.')
 		new_filename = prefix + '.frag.fasta'	
-	if __name__ == '__main__':
-		handle = open(filename, 'r')
-		records = list(SeqIO.parse(handle, "fasta"))
-		record = records[0]
-		records = []
-		for (pos, chunk) in enumerate(chunks(str(record.seq), splitlength)):
-	    		records.append(SeqRecord(Seq(chunk, record.seq.alphabet), id=str(pos), name=record.name, description=record.description))    	
-	 	SeqIO.write(records, open(new_filename, 'w'), "fasta")
+		if __name__ == '__main__':
+			handle = open(filename, 'r')
+			records = list(SeqIO.parse(handle, "fasta"))
+			record = records[0]
+			records = []
+			for (pos, chunk) in enumerate(chunks(str(record.seq), splitlength)):
+	    			records.append(SeqRecord(Seq(chunk, record.seq.alphabet), id=str(pos), name=record.name, description=record.description))    	
+	 		SeqIO.write(records, open(new_filename, 'w'), "fasta")
 
 pla_path = 'pla/*.fasta'
 chr_path = 'chr/*.fasta'
