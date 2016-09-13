b212dfa9081c263bbdcc2b714c12cad5fb6f7b8a
philippmuench <philipp.munech@helmholtz-hzi.de>
2016-09-13 17:23:43 +0200

    update

--- setup.py
+++ setup.py
@@ -1,31 +1,18 @@
-import os
-from setuptools import find_packages
-from setuptools import setup
-import re
+from distutils.core import setup
 
-
-VERSIONFILE=os.path.join('plasmidminer', '_version.py')
-verstrline = open(VERSIONFILE, "rt").read()
-VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
-mo = re.search(VSRE, verstrline, re.M)
-if mo:
-    verstr = mo.group(1)
-else: 
-    raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE,))
-
-long_description = open('README.md', 'r').read()
+version = "0.1"
 
 setup(name='plasmidminer',
-        version = verstr,
-        description='plasmidminer',
-        long_description = long_description,
+        version = version,
+        description='mining of plasmids from metagenomic samples',
+        long_description = 'some text goes here',
         url = 'http://github.com/philippmuench/plasmidminer',
         author='Philipp C. Münch',
         author_email='philipp.muench@helmholtz-hzi.de',
-        license='GNU General Public License, version 3 (GPL-3.0)',
         packages= ['plasmidminer'],
         include_package_data = True,
-        scripts = ['plasmidminer/traitar', 'plasmidminer/merge_preds.py', 'plasmidminer/heatmap.py', 'plasmidminer/domtblout2gene_generic.py', 'traitar/predict.py', 'traitar/hmmer2filtered_best.py', 'traitar/hmm2gff.py'],
+        scripts = ['plasmidminer/plasmidminer', 'plasmidminer/download.py', 'plasmidminer/features.py', 'plasmidminer/simulate_metagenomic_reads.py', 'traitar/simulate_reads.py'],
         zip_safe=False,
-install_requires = ["matplotlib >= 1.3.1", "numpy >= 1.6", "scipy >= 0.13.3", "CGAT >=0.2.5"])
+	install_requires = ["matplotlib >= 1.3.1", "numpy >= 1.6", "scipy >= 0.13.3", "CGAT >=0.2.5"]
+)
 
