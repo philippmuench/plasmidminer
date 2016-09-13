edc6bd51476e1fe0bf5dc4ab49364d3acd5d2c59
philippmuench <philipp.munech@helmholtz-hzi.de>
2016-09-13 14:19:18 +0200

    create functions

--- simulate_reads.py
+++ simulate_reads.py
@@ -1,7 +1,11 @@
 # iterate over all files in pos/ and neg/ folder and generate read-sized fragements
 
-import glob os
+import glob, os
 
+try:
+    from progress.bar import Bar
+except ImportError:
+    print "This script requires progess to be installed!"
 try:
     from Bio import SeqIO
 except ImportError:
@@ -15,57 +19,34 @@ try:
 except ImportError:
     print "This script requires BioPython to be installed!"
 
+def chunks(l, n):
+	for i in xrange(0, len(l), n):
+		yield l[i:i+n]
+
+def split(length, path):
+	files =  glob.iglob(path)
+	#bar = Bar('split plasmid sequences', max=len(files))
+	for filename in files:
+		print(filename)
+		(prefix, sep, suffix) = filename.rpartition('.')
+		new_filename = prefix + '.frag.fasta'	
+	if __name__ == '__main__':
+		handle = open(filename, 'r')
+		records = list(SeqIO.parse(handle, "fasta"))
+		record = records[0]
+		records = []
+		for (pos, chunk) in enumerate(chunks(str(record.seq), splitlength)):
+	    		records.append(SeqRecord(Seq(chunk, record.seq.alphabet), id=str(pos), name=record.name, description=record.description))    	
+	 	SeqIO.write(records, open(new_filename, 'w'), "fasta")
+
+pla_path = 'pla/*.fasta'
+chr_path = 'chr/*.fasta'
+
 splitlength = 200 # set length of outpuf fragments
+split(splitlength, pla_path)
+split(splitlength, chr_path)
 
-# process pos dataset
-for filename in glob.iglob('pla/*.fasta'):
-	print filename
-	(prefix, sep, suffix) = filename.rpartition('.')
- 	new_filename = prefix + '.frag.fasta'
-	try:
-		from Bio import SeqIO
-	except ImportError:
-		print "This script requires BioPython to be installed!"
-
-	def chunks(l, n):
-	 	"""Yield successive n-sized chunks from l."""
-		for i in xrange(0, len(l), n):
-			yield l[i:i+n]
-
-	if __name__ == '__main__':
-		handle = open(filename, 'r')
-		records = list(SeqIO.parse(handle, "fasta"))
-		record = records[0]
-		records = []
-		for (pos, chunk) in enumerate(chunks(record.seq.tostring(), splitlength)):
-	    		records.append(SeqRecord(Seq(chunk, record.seq.alphabet), id=str(pos), name=record.name, description=record.description))
-	 	SeqIO.write(records, open(new_filename, 'w'), "fasta")
-
-# process neg dataset
-for filename in glob.iglob('chr/*.fasta'):
-	print filename
-	(prefix, sep, suffix) = filename.rpartition('.')
- 	new_filename = prefix + '.frag.fasta'
-	try:
-		from Bio import SeqIO
-	except ImportError:
-		print "This script requires BioPython to be installed!"
-
-	def chunks(l, n):
-	 	"""Yield successive n-sized chunks from l."""
-		for i in xrange(0, len(l), n):
-			yield l[i:i+n]
-
-	if __name__ == '__main__':
-		handle = open(filename, 'r')
-		records = list(SeqIO.parse(handle, "fasta"))
-		record = records[0]
-		records = []
-		for (pos, chunk) in enumerate(chunks(record.seq.tostring(), splitlength)):
-	    		records.append(SeqRecord(Seq(chunk, record.seq.alphabet), id=str(pos), name=record.name, description=record.description))
-	 	SeqIO.write(records, open(new_filename, 'w'), "fasta")
-
-# merge to single file
-os.system('cat pla/*.frag.fasta > plasmids_200.fasta')
-os.system('cat chr/*.frag.fasta > chromosomes_200.fasta')
+#print("merge files")
+#os.system('cat pla/*.frag.fasta > plasmids_200.fasta')
+#os.system('cat chr/*.frag.fasta > chromosomes_200.fasta')
 
