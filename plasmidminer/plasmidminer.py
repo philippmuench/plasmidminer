8976dc90175deae5c04817642b9d122a234caee4
philippmuench <philipp.munech@helmholtz-hzi.de>
2016-09-14 11:45:55 +0200

    skip steps

--- plasmidminer/plasmidminer.py
+++ plasmidminer/plasmidminer.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python
-
-import download, simulate
+import os
+import download, simulate, features
 
 # download training/learning dataset
-# TODO(pmuench): test if files exist
 email = "philipp.muench@helmholtz-hzi.de"
-download.downloadChr(email)
-download.downloadPla(email)
+if not os.path.exists('pla'):
+	download.downloadChr(email)
+if not os.path.exists('chr'):
+	download.downloadPla(email)
 
 # get read sized chunks
-# TODO(pmuench): test if files exist
 splitlength = 200 # set length of outpuf fragments
-simulate.split(splitlength, 'pla/*.fasta', 'plasmid_200.fasta')
-simulate.split(splitlength, 'chr/*.fasta', 'chromosomes_200.fasta')
+if not os.path.exists('plasmid_200.fasta'):
+	simulate.split(splitlength, 'pla/*.fasta', 'plasmid_200.fasta')
+if not os.path.exists('chromosomes_200.fasta'):
+	simulate.split(splitlength, 'chr/*.fasta', 'chromosomes_200.fasta')
 
-#TODO(pmuench): get feature matrix
+# get feature matrix
+#if not os.path.exists('plasmid_200.fasta.features'):
+#	features.generate('plasmid_200.fasta')
