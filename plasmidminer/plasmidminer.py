74ddc705e1dbe8968ae68188ca42ebe6a5a42501
philippmuench <philipp.munech@helmholtz-hzi.de>
2016-09-14 12:44:23 +0200

    bugfix

--- plasmidminer/plasmidminer.py
+++ plasmidminer/plasmidminer.py
@@ -4,9 +4,9 @@ import download, simulate, features
 
 # download training/learning dataset
 email = "philipp.muench@helmholtz-hzi.de"
-if not os.path.exists('pla'):
-	download.downloadChr(email)
 if not os.path.exists('chr'):
+	download.downloadChr(email)
+if not os.path.exists('pla'):
 	download.downloadPla(email)
 
 # get read sized chunks
@@ -18,4 +18,7 @@ if not os.path.exists('chromosomes_200.fasta'):
 
 # get feature matrix
 #if not os.path.exists('plasmid_200.fasta.features'):
-#	features.generate('plasmid_200.fasta')
+print('export features for plasmid_200.fasta')
+os.system('plasmidminer/features.py -I plasmid_200.fasta -s length -s na -s cpg > plasmid_200.fasta.features')
+print('export features for chromosomes_200.fasta')
+os.system('plasmidminer/features.py -I chromosomes_200.fasta -s length -s na -s cpg > chromosomes_200.fasta.features')
