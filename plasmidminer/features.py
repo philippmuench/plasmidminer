aa6e515313e362456a2bb1cfa593585f20cdc54f
philippmuench <philipp.munech@helmholtz-hzi.de>
2016-09-15 18:43:54 +0200

    add balancesize()

--- plasmidminer/features.py
+++ plasmidminer/features.py
@@ -10,13 +10,14 @@ try:
 except ImportError:
     print "This script requires GCAT to be installed!"
 
-def removeComments(inputFileName, outputFileName):
+def clearit(inputFileName, outputFileName):
     input = open(inputFileName, "r")
     output = open(outputFileName, "w")
     output.write(input.readline())
     for line in input:
         if not line.lstrip().startswith("#"):
-            output.write(line)
+            if not line.lstrip().startswith('"#'):
+                output.write(line)
     input.close()
     output.close()
 
