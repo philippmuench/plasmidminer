8f8a688acea1400ed7385a9189a27c4499c6c14f
Philipp Muench <philipp.muench@helmholtz-hzi.de>
2016-11-29 18:06:07 +0100

    add oob plot

--- plasmidminer/train.py
+++ plasmidminer/train.py
@@ -1,11 +1,9 @@
-#!/usr/bin/python
-
 from sklearn.ensemble import RandomForestClassifier
 from sklearn import svm
 import numpy as np
 import matplotlib.pyplot as plt
 from numpy import genfromtxt, savetxt, recfromcsv
-import re
+from sklearn.preprocessing import OneHotEncoder
 
 # remove a column from a structured numpy array based on column name
 def remove_field_name(a, name):
@@ -24,7 +22,62 @@ for x in np.nditer(d["lab"]):
     str=np.array_str(x)
     x = str.split("-")[0]
     labels.append(x)
-    
+
+#
 train = remove_field_name(d, "lab")
-train
-#print d.dtype.fields
+
+# convert the structured numpy array to a normal numpy array
+ntrain = train.view(np.float64).reshape(train.shape + (-1,))
+
+rf = RandomForestClassifier(n_estimators=100)
+forest = rf.fit(ntrain, labels)
+
+output = forest.predict(ntrain)
+
+
+# NOTE: Setting the `warm_start` construction parameter to `True` disables
+# support for parallelized ensembles but is necessary for tracking the OOB
+# error trajectory during training.
+ensemble_clfs = [
+    ("RandomForestClassifier, max_features='sqrt'",
+        RandomForestClassifier(warm_start=True, oob_score=True,
+                               max_features="sqrt",
+                               random_state=RANDOM_STATE)),
+    ("RandomForestClassifier, max_features='log2'",
+        RandomForestClassifier(warm_start=True, max_features='log2',
+                               oob_score=True,
+                               random_state=RANDOM_STATE)),
+    ("RandomForestClassifier, max_features=None",
+        RandomForestClassifier(warm_start=True, max_features=None,
+                               oob_score=True,
+                               random_state=RANDOM_STATE))
+]
+
+
+
+# Map a classifier name to a list of (<n_estimators>, <error rate>) pairs.
+error_rate = OrderedDict((label, []) for label, _ in ensemble_clfs)
+
+# Range of `n_estimators` values to explore.
+min_estimators = 15
+max_estimators = 175
+
+for label, clf in ensemble_clfs:
+    for i in range(min_estimators, max_estimators + 1):
+        clf.set_params(n_estimators=i)
+        clf.fit(ntrain, labels)
+
+        # Record the OOB error for each `n_estimators=i` setting.
+        oob_error = 1 - clf.oob_score_
+        error_rate[label].append((i, oob_error))
+
+# Generate the "OOB error rate" vs. "n_estimators" plot.
+for label, clf_err in error_rate.items():
+    xs, ys = zip(*clf_err)
+    plt.plot(xs, ys, label=label)
+
+plt.xlim(min_estimators, max_estimators)
+plt.xlabel("n_estimators")
+plt.ylabel("OOB error rate")
+plt.legend(loc="upper right")
+plt.show()
