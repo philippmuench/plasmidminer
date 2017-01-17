bd672e117a7841f8d0d4c33e4eb634145c6f5c01
anon <philipp.muench@helmholtz-hzi.de>
2017-01-17 13:03:57 +0100

    closes #16

--- plasmidminer/plasmidminer.py
+++ plasmidminer/plasmidminer.py
@@ -67,11 +67,39 @@ def compress():
 		    w = csv.writer(out, delimiter=",")
 		    w.writerows(x for x in csv.reader(inp, delimiter="\t"))
 
+
+def sequence_cleaner(fasta_file, min_length=0, por_n=100):
+	# Create our hash table to add the sequences
+	sequences={}
+	# Using the Biopython fasta parse we can read our fasta input
+	for seq_record in SeqIO.parse(fasta_file, "fasta"):
+        # Take the current sequence
+		sequence = str(seq_record.seq).upper()
+	# Check if the current sequence is according to the user parameters
+	if (len(sequence) >= min_length and
+		(float(sequence.count("N"))/float(len(sequence)))*100 <= por_n):
+		# If the sequence passed in the test "is it clean?" and it isn't in the
+		# hash table, the sequence and its id are going to be in the hash
+		if sequence not in sequences:
+			sequences[sequence] = seq_record.id
+			# If it is already in the hash table, we're just gonna concatenate the ID
+			# of the current sequence to another one that is already in the hash table
+		else:
+			sequences[sequence] += "_" + seq_record.id
+	# Create a file in the same directory where you ran this script
+	output_file = open("clear_" + fasta_file, "w+")
+	# Just read the hash table and write on the file as a fasta format
+	for sequence in sequences:
+	output_file.write(">" + sequences[sequence] + "\n" + sequence + "\n")
+	output_file.close()
+	print("CLEAN!!!\nPlease check clear_" + fasta_file)
+
+
 def getkmerprofile():
 	# get feature matrix
 	print('export features')
 	if not os.path.exists('dat/train.features'):
-	    os.system('python plasmidminer/features.py -I dat/train.fasta -s length -s na -s cpg > dat/train.features')
+	    os.system('python plasmidminer/features.py -I dat/clear_train.fasta -s length -s na -s cpg > dat/train.features')
 	    print ('export csv')
 	    with open("dat/train.features", "r") as inp, open("dat/train.features.csv", "w") as out:
 		w = csv.writer(out, delimiter=",")
@@ -95,5 +123,6 @@ if __name__ == "__main__":
 	getchunks()
 	getstatfeatures()
 	compress()
+	sequence_cleaner('dat/train.fasta', args.chunksize, por_n=100)
 	getkmerprofile()
 
