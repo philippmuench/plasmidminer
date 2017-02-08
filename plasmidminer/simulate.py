dbabf1ce56c9d417b7f2a5775b6e1b677342d72a
anon <philipp.muench@helmholtz-hzi.de>
2017-02-08 13:12:05 +0100

    add read simulation function and fastq converter

--- plasmidminer/simulate.py
+++ plasmidminer/simulate.py
@@ -3,19 +3,21 @@
 
 import glob, os, random, sys
 from termcolor import colored
+import sys, os
+from Bio import SeqIO
 
 try:
-    from Bio import SeqIO
-    from Bio.Seq import Seq
-    from Bio.SeqRecord import SeqRecord
+	from Bio import SeqIO
+	from Bio.Seq import Seq
+	from Bio.SeqRecord import SeqRecord
 except ImportError:
-    print "This script requires BioPython to be installed!"
+	print "This script requires BioPython to be installed!"
 
 class Printer():
-    """Print things to stdout on one line dynamically"""
-    def __init__(self,data):
-        sys.stdout.write("\r\x1b[K"+data.__str__())
-        sys.stdout.flush()
+	"""Print things to stdout on one line dynamically"""
+	def __init__(self,data):
+		sys.stdout.write("\r\x1b[K"+data.__str__())
+		sys.stdout.flush()
 
 def chunks(l, n):
 	for i in xrange(0, len(l), n):
@@ -62,21 +64,41 @@ def split(length, path, export, chunkspath):
 		record = records[0]
 		records = []
 		for (pos, chunk) in enumerate(chunks(str(record.seq), length)):
-	    		records.append(SeqRecord(Seq(chunk, record.seq.alphabet), id=str(pos), name=record.name, description=record.description))    	
-	 	SeqIO.write(records, open(new_filename, 'w'), "fasta")
-	 	# create multiple sequence fasta file
+				records.append(SeqRecord(Seq(chunk, record.seq.alphabet), id=str(pos), name=record.name, description=record.description))    	
+		SeqIO.write(records, open(new_filename, 'w'), "fasta")
+		# create multiple sequence fasta file
 	Printer(colored('exporting: ' + export, 'green'))
 	#sprint("exporting " + export)
-    	with open(export, 'w') as outfile:
-        	for fname in glob.iglob(chunkspath):
-        		with open(fname) as infile:
-        			for line in infile:
-        				outfile.write(line)
+	with open(export, 'w') as outfile:
+		for fname in glob.iglob(chunkspath):
+			with open(fname) as infile:
+				for line in infile:
+					outfile.write(line)
 	Printer(colored('cleanup', 'green'))
 	(prefix, sep, suffix) = path.rpartition('.')
 	filelist = glob.glob(prefix + '.frag.fasta') 
 	for f in filelist:
 		os.remove(f)
+
+def readsim(chunksize, simnum, input_file, output_file):
+	Printer(colored('simulate reads of ' + input_file, 'green'))
+	output_file_fq = output_file + '.fq'
+	output_file2_q = output_file + 'fq.l2'
+	s = " "
+	cmd = ("src/wgsim -e 0 -r 0 -R 0 -X 0 -A 0 -N", str(simnum), "-1", str(chunksize) , str(input_file), str(output_file_fq), str(output_file2_q))
+	print(s.join( cmd ))
+	os.system(s.join( cmd ))
+	confertfastqtofasta(output_file_fq, output_file)
+
+def confertfastqtofasta(fq_path, fa_path):
+	# conert fastq to fasta
+	input_handle = open(fq_path, "rU")
+	output_handle = open(fa_path, "w")
+	sequences = SeqIO.parse(input_handle, "fastq")
+	count = SeqIO.write(sequences, output_handle, "fasta")
+	output_handle.close()
+	input_handle.close()
+
 if __name__ == "__main__":
 	sys.exit(main(sys.argv))
-    
+	
